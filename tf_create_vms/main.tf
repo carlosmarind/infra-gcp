b38dfff0471f021b04babbefb4bc0bd350885cbb
Carlos Marin Diaz <carlosmarind@gmail.com>
2024-09-11 11:40:21 -0300

    actualizacion de vms

--- tf_create_vms/main.tf
+++ tf_create_vms/main.tf
@@ -13,19 +13,12 @@ resource "google_compute_instance" "default" {
     network = "default"
     access_config {}
   }
-  metadata_startup_script = templatefile("startup.sh", { username = element(var.instance_tag, count.index), password = "${element(var.instance_tag, count.index)}-${var.ssh_password}", instance = count.index, sa-config = google_service_account_key.key.private_key, region = var.region_a })
-  tags                    = [var.lab_tag]
-}
-
-output "ip" {
-  value = google_compute_instance.default[*].network_interface.0.access_config.0.nat_ip
-}
-
-data "google_service_account" "sa-account" {
-  account_id = "sa-registry"
-}
-## Artifact Registry
-resource "google_service_account_key" "key" {
-  service_account_id = data.google_service_account.sa-account.id
-  public_key_type    = "TYPE_X509_PEM_FILE"
+  metadata_startup_script = templatefile("scripts/startup.sh", {
+    username  = element(var.instance_tag, count.index),
+    password  = "${element(var.instance_tag, count.index)}-${var.ssh_password}",
+    instance  = count.index,
+    sa-config = data.google_service_account_key.sa-account-key.public_key,
+    region    = var.region_a
+  })
+  tags = [var.lab_tag]
 }
