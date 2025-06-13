e3f12a1b5c29d25508d557bde6f3df7b19837f35
Carlos Marin Diaz <carlosmarind@gmail.com>
2025-06-13 17:56:14 -0400

    actualizacion de infra para creacion de servidor de laboratorio

--- tf_create_vms_v2/output.tf
+++ tf_create_vms_v2/output.tf
@@ -1,3 +1,9 @@
-output "ip" {
+output "public_ip" {
   value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+}
+output "public_dns" {
+  value = google_dns_record_set.devops_cl.name
+}
+output "public_dns_record_value" {
+  value = google_dns_record_set.devops_cl.rrdatas
 }
